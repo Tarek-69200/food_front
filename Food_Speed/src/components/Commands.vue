e617d1628f4b3ddde59cb9b202840b3024cc223a
Tarek Taguine <t.taguine@it-students.fr>
2024-03-07 22:02:02 +0100

    v:0.0.1 - Create props for commande

--- Food_Speed/src/components/Commands.vue
+++ Food_Speed/src/components/Commands.vue
@@ -1,10 +1,6 @@
 <script setup lang="ts">
 import WelcomeItem from './WelcomeItem.vue'
-import DocumentationIcon from './icons/IconDocumentation.vue'
-import ToolingIcon from './icons/IconTooling.vue'
-import EcosystemIcon from './icons/IconEcosystem.vue'
-import CommunityIcon from './icons/IconCommunity.vue'
-import SupportIcon from './icons/IconSupport.vue'
+
 </script>
 
 <template>
@@ -14,32 +10,8 @@ import SupportIcon from './icons/IconSupport.vue'
         title="Documentation"
         text="Find in-depth information about Vue features and API."
         >
-        <DocumentationIcon />
         </WelcomeItem>
-        <WelcomeItem
-        title="Tooling"
-        text="Discover tooling and recommendations on how to use them."
-        >
-        <ToolingIcon />
-        </WelcomeItem>
-        <WelcomeItem
-        title="Ecosystem"
-        text="Learn about the libraries and official projects that make up the Vue ecosystem."
-        >
-        <EcosystemIcon />
-        </WelcomeItem>
-        <WelcomeItem
-        title="Community"
-        text="Join a welcoming and diverse community."
-        >
-        <CommunityIcon />
-        </WelcomeItem>
-        <WelcomeItem
-        title="Support"
-        text="Find out how to get help from the community and maintainers."
-        >
-        <SupportIcon />
-        </WelcomeItem>
+        
     </div>
 </template>
 
