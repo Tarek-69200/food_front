24dbfb51caa783d495c8cffe7a731a627bb3e7e8
Tarek Taguine <t.taguine@it-students.fr>
2024-03-07 21:36:10 +0100

    v:0.0.1 - Create route for commands

---
+++ Food_Speed/src/views/CommandeView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="commande">
+        <h1>Commande réalisée</h1>
+      
+    </div>
+
+</template>
+
+<style>
+
+@media (min-width: 1024px) {
+    .commande {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+    }
+}
+
+</style>
